c83c7378ed9952f871e82b3a47117f2c207631c5
wwwtamil14789-anbu <www.tamil14789@gmail.com>
2026-05-20 16:57:49 +0530

    Commit

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,5 +1,12 @@
 import d1 from './imgs/about.jpg';
 
+import e1 from './imgs/property-1.jpg';
+import e2 from './imgs/property-2.jpg';
+import e3 from './imgs/property-3.jpg';
+import e4 from './imgs/property-4.jpg';
+import e5 from './imgs/property-5.jpg';
+import e6 from './imgs/property-6.jpg';
+
 const About = () => {
     return (
         <>
@@ -71,6 +78,80 @@ const About = () => {
                             </div>
                         </div>
                     </div>
+
+                    <footer class="footer pt-5">
+                <div class="container">
+                    <div class="row g-5">
+
+                        {/* <!-- Get In Touch --> */}
+                        <div class="col-lg-3 col-md-6">
+                            <h4 class="footer-title">Get In Touch</h4>
+                            <p><i class="fa fa-map-marker-alt me-2"></i>123 Street, New York, USA</p>
+                            <p><i class="fa-solid fa-phone"></i>  +012 345 67890</p>
+                            <p><i class="fa fa-envelope me-2"></i>makaan@gmail.com</p>
+
+                            <div class="footer-social mt-4">
+                                <a href="#"><i class="fab fa-twitter"></i></a>
+                                <a href="#"><i class="fab fa-facebook-f"></i></a>
+                                <a href="#"><i class="fab fa-youtube"></i></a>
+                                <a href="#"><i class="fab fa-linkedin-in"></i></a>
+                            </div>
+                        </div>
+
+                        {/* <!-- Quick Links --> */}
+                        <div class="col-lg-3 col-md-6">
+                            <h4 class="footer-title">Quick Links</h4>
+                            <ul class="footer-links">
+                                <li><a href="#"><i class="fa-solid fa-angle-right"></i> About Us</a></li>
+                                <li><a href="#"><i class="fa-solid fa-angle-right"></i> Contact Us</a></li>
+                                <li><a href="#"><i class="fa-solid fa-angle-right"></i> Our Services</a></li>
+                                <li><a href="#"><i class="fa-solid fa-angle-right"></i> Privacy Policy</a></li>
+                                <li><a href="#"><i class="fa-solid fa-angle-right"></i> Terms & Condition</a></li>
+                            </ul>
+                        </div>
+
+                        {/* <!-- Photo Gallery --> */}
+                        <div class="col-lg-3 col-md-6">
+                            <h4 class="footer-title">Photo Gallery</h4>
+                            <div class="row g-2">
+                                <div class="col-4"><img src={e1} class="img-fluid gallery-img" /></div>
+                                <div class="col-4"><img src={e2} class="img-fluid gallery-img" /></div>
+                                <div class="col-4"><img src={e3} class="img-fluid gallery-img" /></div>
+                                <div class="col-4"><img src={e4} class="img-fluid gallery-img" /></div>
+                                <div class="col-4"><img src={e5} class="img-fluid gallery-img" /></div>
+                                <div class="col-4"><img src={e6} class="img-fluid gallery-img" /></div>
+                            </div>
+                        </div>
+
+                        {/* <!-- Newsletter --> */}
+                        <div class="col-lg-3 col-md-6">
+                            <h4 class="footer-title">Newsletter</h4>
+                            <p>Dolor amet sit justo amet elitr clita ipsum elitr est.</p>
+                            <div class="position-relative">
+                                <input type="email" class="form-control footer-input" placeholder="Your email" />
+                                <button class="btn footer-btn">SignUp</button>
+                            </div>
+                        </div>
+
+                    </div>
+
+                    <hr class="footer-line my-4" />
+
+                    {/* <!-- Bottom --> */}
+                    <div class="row align-items-center pb-3">
+                        <div class="col-md-6 text-center text-md-start">
+                            <small>© <span class="fw-bold">Your Site Name</span>, All Right Reserved. Designed By <span class="fw-bold">HTML Codex</span></small>
+                        </div>
+                        <div class="col-md-6 text-center text-md-end">
+                            <a href="#" class="footer-bottom-link">Home</a>
+                            <a href="#" class="footer-bottom-link">Cookies</a>
+                            <a href="#" class="footer-bottom-link">Help</a>
+                            <a href="#" class="footer-bottom-link">FAQs</a>
+                        </div>
+                    </div>
+
+                </div>
+            </footer>
         </>
     )
 }
