0edf3705ab1e4aa57b5e8ac595cb448962801370
wwwtamil14789-anbu <www.tamil14789@gmail.com>
2026-05-20 16:14:37 +0530

    Commit

--- src/pages/Propertyagent.js
+++ src/pages/Propertyagent.js
@@ -1,6 +1,193 @@
 const Propertyagent  = () => {
     return (
-        <center><h2>Property Agent</h2></center>
+        <>
+        <section class="agent-section py-5">
+                <div class="container">
+                    <div class="agent-box p-4 p-lg-5">
+                        <div class="row align-items-center">
+
+                            {/* <!-- Image --> */}
+                            <div class="image col-lg-6 mb-4 mb-lg-0">
+                                <img src={f1} class="img-fluid rounded-3" alt="Agent" />
+                            </div>
+
+                            {/* <!-- Content --> */}
+                            <div class="agent-content">
+                                <h1>Contact With Our Certified <br /> Agent</h1>
+                                <p>
+                                    Eirmod sed ipsum dolor sit rebum magna erat. Tempor lorem kasd vero ipsum <br />
+                                    sit sit diam justo sed vero dolor duo.
+                                </p>
+
+
+                                <div class="agent-buttons">
+                                    <a href="#" class="btn-call">
+                                        <i class="fa-solid fa-phone"></i>
+                                        Make A Call
+                                    </a>
+
+                                    <a href="#" class="btn-appointment">
+                                        <i class="fa-solid fa-calendar-days"></i>
+                                        Get Appointment
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section >
+
+
+            <section class="py-5">
+                <div class="container">
+
+                    {/* <!-- Section Title --> */}
+                    <div class="text-center mb-5">
+                        <h1 class="fw-bold">Property Agents</h1>
+                        <p class="text-muted mt-3">
+                            Eirmod sed ipsum dolor sit rebum labore magna erat. Tempor ut dolore lorem kasd<br />
+                            vero ipsum sit eirmod sit. Ipsum diam justo sed rebum vero dolor duo.
+                        </p>
+                    </div>
+
+                    {/* <!-- Agents --> */}
+                    <div class="row g-4">
+
+                        {/* <!-- Agent Card --> */}
+                        <div class="col-lg-3 col-md-6">
+                            <div class="card border-0 shadow text-center agent-card">
+                                <img src={g1} class="card-img-top" alt="" />
+                                <div class="card-body">
+                                    <div class="agent-social">
+                                        <a href="#"><i class="fab fa-facebook-f"></i></a>
+                                        <a href="#"><i class="fab fa-twitter"></i></a>
+                                        <a href="#"><i class="fab fa-instagram"></i></a>
+                                    </div>
+                                    <h5 class="fw-bold mt-4">Full Name</h5>
+                                    <small class="text-muted">Designation</small>
+                                </div>
+                            </div>
+                        </div>
+
+                        {/* <!-- Copy same card --> */}
+                        <div class="col-lg-3 col-md-6">
+                            <div class="card border-0 shadow text-center agent-card">
+                                <img src={g2} class="card-img-top" alt="" />
+                                <div class="card-body">
+                                    <div class="agent-social">
+                                        <a href="#"><i class="fab fa-facebook-f"></i></a>
+                                        <a href="#"><i class="fab fa-twitter"></i></a>
+                                        <a href="#"><i class="fab fa-instagram"></i></a>
+                                    </div>
+                                    <h5 class="fw-bold mt-4">Full Name</h5>
+                                    <small class="text-muted">Designation</small>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-lg-3 col-md-6">
+                            <div class="card border-0 shadow text-center agent-card">
+                                <img src={g3} class="card-img-top" alt="" />
+                                <div class="card-body">
+                                    <div class="agent-social">
+                                        <a href="#"><i class="fab fa-facebook-f"></i></a>
+                                        <a href="#"><i class="fab fa-twitter"></i></a>
+                                        <a href="#"><i class="fab fa-instagram"></i></a>
+                                    </div>
+                                    <h5 class="fw-bold mt-4">Full Name</h5>
+                                    <small class="text-muted">Designation</small>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-lg-3 col-md-6">
+                            <div class="card border-0 shadow text-center agent-card">
+                                <img src={g4} class="card-img-top" alt="" />
+                                <div class="card-body">
+                                    <div class="agent-social">
+                                        <a href="#"><i class="fab fa-facebook-f"></i></a>
+                                        <a href="#"><i class="fab fa-twitter"></i></a>
+                                        <a href="#"><i class="fab fa-instagram"></i></a>
+                                    </div>
+                                    <h5 class="fw-bold mt-4">Full Name</h5>
+                                    <small class="text-muted">Designation</small>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </section>
+
+
+            <section class="py-5 bg-white">
+                <div class="container">
+
+                    {/* <!-- Title --> */}
+                    <div class="text-center mb-5">
+                        <h1 class="fw-bold">Our Clients Say!</h1>
+                        <p class="text-muted mt-3">
+                            Eirmod sed ipsum dolor sit rebum labore magna erat. Tempor ut dolore lorem kasd<br />
+                            vero ipsum sit eirmod sit. Ipsum diam justo sed rebum vero dolor duo.
+                        </p>
+                    </div>
+
+                    {/* <!-- Testimonials --> */}
+                    <div class="testimonial-wrapper">
+
+                        {/* <!-- Item --> */}
+                        <div class="testimonial-item">
+                            <p>
+                                Tempor stet labore dolor clita stet diam amet ipsum dolor duo ipsum rebum stet dolor
+                                amet diam stet. Est stet ea lorem amet est kasd kasd erat eos
+                            </p>
+
+                            <div class="d-flex align-items-center mt-3">
+                                <img src={h1} class="rounded-circle" alt="" />
+                                <div class="ms-3">
+                                    <h6 class="mb-0 fw-bold">Client Name</h6>
+                                    <small class="text-muted">Profession</small>
+                                </div>
+                            </div>
+                        </div>
+
+                        {/* <!-- Item --> */}
+                        <div class="testimonial-item">
+                            <p>
+                                Tempor stet labore dolor clita stet diam amet ipsum dolor duo ipsum rebum stet dolor
+                                amet diam stet. Est stet ea lorem amet est kasd kasd erat eos
+                            </p>
+
+                            <div class="d-flex align-items-center mt-3">
+                                <img src={h2} class="rounded-circle" alt="" />
+                                <div class="ms-3">
+                                    <h6 class="mb-0 fw-bold">Client Name</h6>
+                                    <small class="text-muted">Profession</small>
+                                </div>
+                            </div>
+                        </div>
+
+                        {/* <!-- Item --> */}
+                        <div class="testimonial-item">
+                            <p>
+                                Tempor stet labore dolor clita stet diam amet ipsum dolor duo ipsum rebum stet dolor
+                                amet diam stet. Est stet ea lorem amet est kasd kasd erat eos
+                            </p>
+
+                            <div class="d-flex align-items-center mt-3">
+                                <img src={h3} class="rounded-circle" alt="" />
+                                <div class="ms-3">
+                                    <h6 class="mb-0 fw-bold">Client Name</h6>
+                                    <small class="text-muted">Profession</small>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </section>
+            </>
+
     )
 }     
 export default Propertyagent;
